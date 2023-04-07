e7c92994c38687959bbbcba25cc101c52b96723b
Zachary Casares <rrumble250@gmail.com>
2023-04-07 18:51:03 -0400

    #136 fix

--- src/Components/Meals/MealItemForm.jsx
+++ src/Components/Meals/MealItemForm.jsx
@@ -1,13 +1,13 @@
 import "./MealItemForm.css";
 import Input from "../Ui/Input.jsx";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
   return (
     <div className="form">
       <Input
         label="Amount"
         input={{
-          id: "amount",
+          id: "amount_" + props.id,
           type: "number",
           min: "1",
           max: "5",
